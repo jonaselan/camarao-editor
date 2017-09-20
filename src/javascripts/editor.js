301797c949df3cf85c381e3f749db6e6d4d71685
jonaselan <jonas.elan@gmail.com>
2017-09-20 14:48:08 -0300

    Add unminify Ace dependencies
    Is important for study how is hightlight is done here.

--- src/javascripts/editor.js
+++ src/javascripts/editor.js
@@ -3,50 +3,7 @@ var fs = require('fs');
 
 var currentWindow  = remote.getCurrentWindow();
 var JavaScriptMode = ace.require('ace/mode/javascript').Mode;
-var editorInstance = ace.edit('editor');
+var editor = ace.edit('editor');
 
-// const template = [
-//   {
-//     label: 'Open',
-//     submenu: [
-//       {
-//         label: 'sim',
-//         click: openHandler
-//       }
-//     ]
-//   },
-//   {
-//     label: 'View',
-//     submenu: [
-//       {
-//         label: 'Save As',
-//         click: saveHandler
-//       }
-//     ]
-//   }
-// ]
-//
-// var magit inMenu = Menu.buildFromTemplate(template);
-// Menu.setApplicationMenu(mainMenu);
-
-function openHandler () {
-    var fileNames = dialog.showOpenDialog(currentWindow);
-
-    if (fileNames !== undefined) {
-        var fileName = fileNames[0];
-        fs.readFile(fileName, 'utf8', function (err, data) {
-            editorInstance.setValue(data);
-        });
-    }
-}
-
-function saveHandler () {
-    var fileName = dialog.showSaveDialog(currentWindow);
-
-    if (fileName !== undefined) {
-        fs.writeFile(fileName, editorInstance.getValue());
-    }
-}
-
-editorInstance.setTheme('ace/theme/twilight');
-editorInstance.session.setMode(new JavaScriptMode());
+editor.setTheme('ace/theme/twilight');
+editor.session.setMode(new JavaScriptMode());
